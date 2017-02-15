9d1ed5c0af55939474de00c4c5adc075a40f2dee
MiqueasLH <miqueaslaynas@gmail.com>
2017-02-15 00:19:41 -0800

    Complete guess check function

--- game.rb
+++ game.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 # API call to LI words list
-words = open("http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words")
+words = open('http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words')
 res_bod = words.read
 # Format words into array
 list_of_words = res_bod.split(' ')
@@ -11,15 +11,33 @@ answer = rando_word.split('')
 # Answer status the user can see
 secret_word = []
 # Set the right amount of "-"s for the user to see
-answer.length.times do |i|
-  secret_word << "-"
+answer.length.times do |_i|
+    secret_word << '-'
 end
 # set the letters guessed list container
 guessed_letters = []
 # set remaining number of guesses
 guesses = 6
 # create a method to check if guessed letter is in the answer
-def guess_checker(answer_arr,guess)
-
+def guess_checker(answer_arr, guess, secret_arr)
+    counter = 0
+    initial_secret_array = secret_arr
+    for let in answer_arr
+        secret_arr[counter] = let if let == guess
+        counter += 1
+    end
+    if secret_arr == initial_secret_array
+        puts 'Wrong!'
+    else
+        puts 'Correct!'
+    end
 end
-p secret_word
+puts 'Welcome to the hangman games'
+
+until guesses == 0 || answer == secret_word
+  puts "You've used the following letters #{guessed_letters.join(', ')}" unless guessed_letters.empty?
+  puts "You have #{guesses} guesses left, pick a letter"
+  guess = gets.chomp.downcase
+
+  break
+end
